fa119f38c065c4c3a0f75d4d1ff62215097929f1
asliAdarsh <adarshjaiswal9966@gmail.com>
2025-09-14 14:22:36 +0530

    Signup controllers refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,13 @@
 import jwt from 'jsonwebtoken';
-
+import { ENV } from "./env.js";
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: '7d',
+    const { JWT_SECRET } = ENV;
+    if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+    }
+
+    const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "7d",
     });
     res.cookie('jwt', token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
