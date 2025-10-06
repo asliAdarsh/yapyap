ac4a67e9ac5512461229d5dd7c0a4db648b72a9d
asliAdarsh <adarshjaiswal9966@gmail.com>
2025-10-06 18:19:29 +0530

    login logout implementation

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -66,3 +66,40 @@ export const signup = async (req, res) => {
     }
 
 }
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+     if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+    try {
+      const user = await User.findOne({email})
+      if(!user){
+        return res.status(400).json({message: 'Invalid email or password'})
+      }
+      
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if(!isPasswordValid){
+        return res.status(400).json({message: 'Invalid email or password'})
+      }
+    generateToken(user._id, res);
+    return res.status(200).json(
+        { 
+            message: 'User logged in successfully',
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        }
+    );
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
+};
